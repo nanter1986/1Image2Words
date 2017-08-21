4cfea5e7cc2a273a8d2e79a0f632ea70e835af8a
nanter1986 <nanter1986@gmail.com>
2017-08-21 12:13:35 +0300

    Initial commit

---
+++ app/src/main/java/com/oneimagetwowords/nanter1986/a1image2words/MainActivity.java
@@ -0,0 +1,13 @@
+package com.oneimagetwowords.nanter1986.a1image2words;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
